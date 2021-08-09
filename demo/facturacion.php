88e14c4e9366279ac81fd63443faea61c735d6b5
Edmundo Guerrero <eguerrero@oficsa.com>
2021-08-08 19:30:44 -0600

    Facturacion corregido

--- demo/facturacion.php
+++ demo/facturacion.php
@@ -134,15 +134,15 @@
                 <div class="page-title">
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Clientes</h3>
-                            <p class="text-subtitle text-muted">Listado de todos los clientes</p>
+                            <h3>Facturacion</h3>
+                            <p class="text-subtitle text-muted">Listado de todos las facturas</p>
                         </div>
                     </div>
                 </div>
                 <section class="section">
                     <div class="card">
                         <div class="card-header">
-                            Clientes
+                            Facturacion
                             <a href="#" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#aggfactura">Agregar</a>
                         </div>
                         <div class="card-body">
